bd020b06358d2110bb2886388c0771100378b0df
illyir <none110500@gmail.com>
2022-04-17 17:36:36 +0700

    asd

--- app/src/main/java/com/secorp/rus_quest/MainActivity.kt
+++ app/src/main/java/com/secorp/rus_quest/MainActivity.kt
@@ -2,10 +2,35 @@ package com.secorp.rus_quest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import io.ktor.client.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {    val client = HttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+           var button = findViewById<Button>(R.id.button)
+            button.setOnClickListener {
+                val id = 3
+                MainScope().launch(Dispatchers.IO) {
+                    val data = client.get<String>("http://192.168.111.164:8080/task/$id"){
+                    }
+                    withContext(Dispatchers.Main){
+                        var zxc = findViewById<TextView>(R.id.zxc)
+                        zxc.text = data
+                    }
+                    Log.d("Simple case ", data)
+            }
+
+
+
+        }
     }
 }
